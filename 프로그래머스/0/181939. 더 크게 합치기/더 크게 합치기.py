26d659257d2f6370ea971387eab5922ac4a05db2
LHANTAEK <143519383+LHANTAEK@users.noreply.github.com>
2024-06-28 00:00:41 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
@@ -1,15 +1,19 @@
 def solution(a, b):
     answer = 0
     if a < b:
-        if str(a) + str(b) < str(b) + str(a):
+        if int(str(a) + str(b)) < int(str(b) + str(a)):
             answer = int(str(b) + str(a))
         else:
             answer = int(str(a) + str(b))
             
     else:
-        if str(a) + str(b) < str(b) + str(a):
+        if int(str(a) + str(b)) < int(str(b) + str(a)):
             answer = int(str(b) + str(a))
         else:
             answer = int(str(a) + str(b))
                   
     return answer
+
+# max(a,b)를 사용해서 풀 수도 있다!
+# def solution(a,b):
+#     return int(max(f"{a}{b}", f"{b}{a}"))
