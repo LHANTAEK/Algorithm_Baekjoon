58daf9b32d31d68d90bb8bb471f4ddf181aa4bee
LHANTAEK <143519383+LHANTAEK@users.noreply.github.com>
2024-07-03 00:29:19 +0900

    [level 0] Title: 수 조작하기 1, Time: 2.97 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
@@ -11,3 +11,13 @@ def solution(n, control):
             n += -10
 
     return n
+
+
+# 키-밸류 dic 으로 문제를 풀 수도 있다.
+
+# def solution(n, control):
+#     answer = n
+#     c = { 'w':1, 's':-1, 'd':10, 'a':-10}
+#     for i in control:
+#         answer += c[i]
+#     return answer
