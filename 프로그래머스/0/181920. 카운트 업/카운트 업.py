ce794534f7143b89677da948eb42a099f7c4ebd2
LHANTAEK <143519383+LHANTAEK@users.noreply.github.com>
2024-07-17 04:18:54 +0900

    [level 0] Title: 카운트 업, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.py
@@ -0,0 +1,12 @@
+def solution(start_num, end_num):
+    answer = []
+    for i in range(start_num, end_num + 1):
+        answer.append(i)
+    return answer
+
+
+
+# 다른 풀이
+
+# def solution(start_num, end_num):
+#     return list(range(start_num, end_num +1))
