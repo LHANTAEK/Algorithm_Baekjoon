151fc572f5e194546fb2c9d795c1e16c878eab59
LHANTAEK <143519383+LHANTAEK@users.noreply.github.com>
2024-07-28 14:31:23 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.15 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
@@ -0,0 +1,15 @@
+def solution(my_string):
+    answer = []
+    dic = {}
+    for i in 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz':
+        dic[i] = 0
+        
+    for i in my_string:
+        if i in dic:
+            dic[i] += 1
+        else:
+            dic[i] = 1
+            
+    answer = list(dic.values())
+
+    return answer
