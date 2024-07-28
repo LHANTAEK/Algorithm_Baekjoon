c66077ab9abaa9c584d94b25833a6527902e6122
LHANTAEK <143519383+LHANTAEK@users.noreply.github.com>
2024-07-28 17:09:03 +0900

    [level 0] Title: 카운트 다운, Time: 0.03 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.py
@@ -0,0 +1,7 @@
+def solution(start_num, end_num):
+    answer = []
+    for i in range(end_num, start_num+1):
+        answer.append(i)
+        answer.sort(reverse=True)        
+        
+    return answer
