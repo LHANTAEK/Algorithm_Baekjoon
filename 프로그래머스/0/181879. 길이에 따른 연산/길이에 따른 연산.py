9fc5125106afb5a14894b1ac2416c1b107b55766
LHANTAEK <143519383+LHANTAEK@users.noreply.github.com>
2024-07-30 10:24:00 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.py
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.py
@@ -1,9 +1,19 @@
+# def solution(num_list):
+#     answer = 0
+#     if len(num_list) >= 11:
+#         answer = sum(num_list)
+#     else:
+#         for i in range(1, len(num_list)):
+#             num_list[i] = num_list[i] * num_list[i-1]
+#             answer = num_list[-1]
+#     return answer
+
+
+# 다른 풀이
 def solution(num_list):
-    answer = 0
     if len(num_list) >= 11:
-        answer = sum(num_list)
+        answer = eval('+'.join(list(map(str, num_list))))
+        return answer
     else:
-        for i in range(1, len(num_list)):
-            num_list[i] = num_list[i] * num_list[i-1]
-            answer = num_list[-1]
-    return answer
+        answer = eval('*'.join(list(map(str, num_list))))
+        return answer
