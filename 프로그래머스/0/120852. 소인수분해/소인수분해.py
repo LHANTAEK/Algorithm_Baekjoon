5883cf4ceed6e1078a156263c2b07f06c3879054
임한택_T7429 <143519383+LHANTAEK@users.noreply.github.com>
2024-08-20 00:03:31 +0900

    [level 0] Title: 소인수분해, Time: 0.82 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.py
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.py
@@ -1,13 +1,11 @@
 def solution(n):
     answer = []
     i = 2
-    while i * i <= n:
+    while i <= n:
         if n % i == 0:
-            if i not in answer:
-                answer.append(i)
-            n //= i
+            answer.append(i)
+            n = n // i
         else:
             i += 1
-    if n > 1 and n not in answer:  # n이 1보다 크다면 마지막으로 남은 소수임
-        answer.append(n)
+    answer = list(set(answer))
     return sorted(answer)
