916998e3bbed716a3c0a3feb00d03eabcb5e8cb0
임한택_T7429 <143519383+LHANTAEK@users.noreply.github.com>
2024-09-27 23:27:01 +0900

    [level 0] Title: 종이 자르기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120922. 종이 자르기/종이 자르기.py
@@ -0,0 +1,9 @@
+def solution(M, N):
+    answer = 0
+    if M == 1 and N == 1:
+        answer = 0
+    elif M >= N:
+        answer = (N-1) + N*(M-1)
+    else:
+        answer = (M-1) + M*(N-1)
+    return answer
