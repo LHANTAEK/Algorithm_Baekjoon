9cfb54f70e3ba8c6f982422c169b3eaaea985f5a
임한택_T7429 <143519383+LHANTAEK@users.noreply.github.com>
2024-10-01 23:51:47 +0900

    [level 1] Title: 신고 결과 받기, Time: 215.95 ms, Memory: 52.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -0,0 +1,17 @@
+def solution(id_list, report, k):
+    
+    reported = {id: set() for id in id_list}
+    
+    
+    for r in set(report):
+        reporter, reported_user = r.split()
+        reported[reported_user].add(reporter)
+    
+    
+    answer = {id: 0 for id in id_list}
+    for reported_user, reporters in reported.items():
+        if len(reporters) >= k: 
+            for reporter in reporters:
+                answer[reporter] += 1
+    
+    return [answer[id] for id in id_list]
