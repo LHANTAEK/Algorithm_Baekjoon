995a59d47663745f56f52840848edca0c94a8c9b
임한택_T7429 <143519383+LHANTAEK@users.noreply.github.com>
2024-10-12 22:14:14 +0900

    [level 1] Title: 최소직사각형, Time: 8.90 ms, Memory: 10.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -0,0 +1,14 @@
+def solution(sizes):
+    max_width = 0
+    max_height = 0
+    
+    for card in sizes:
+        # 각 명함의 긴 쪽을 가로로, 짧은 쪽을 세로로 재배치
+        width, height = max(card), min(card)
+        
+        # 현재까지의 최대 가로, 세로 길이와 비교하여 갱신
+        max_width = max(max_width, width)
+        max_height = max(max_height, height)
+    
+    # 가장 큰 가로 길이와 세로 길이를 곱하여 지갑의 크기 반환
+    return max_width * max_height
