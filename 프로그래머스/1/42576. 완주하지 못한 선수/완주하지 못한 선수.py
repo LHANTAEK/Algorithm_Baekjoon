68c92e0dbe358826e3f3a16ad48d08a2c355cec7
임한택_T7429 <143519383+LHANTAEK@users.noreply.github.com>
2024-10-14 00:14:50 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 65.47 ms, Memory: 38.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,17 +1,24 @@
 def solution(participant, completion):
-    answer = ''
-    participant_dict = {}
+#     answer = ''
+#     participant_dict = {}
     
-    for name in participant:
-        if name in participant_dict:
-            participant_dict[name] += 1
-        else:
-            participant_dict[name] = 1
+#     for name in participant:
+#         if name in participant_dict:
+#             participant_dict[name] += 1
+#         else:
+#             participant_dict[name] = 1
             
-    for complete_name in completion:
-        participant_dict[complete_name] -= 1
+#     for complete_name in completion:
+#         participant_dict[complete_name] -= 1
         
-    for i in participant:
-        if participant_dict[i] == 1:
-            answer = i
-    return answer
+#     for i in participant:
+#         if participant_dict[i] == 1:
+#             answer = i
+            
+#     return answer
+
+    # 다른 풀이
+    from collections import Counter
+    
+    answer = Counter(participant) - Counter(completion)
+    return list(answer.keys())[0]
