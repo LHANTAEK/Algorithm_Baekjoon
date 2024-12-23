195bc201e1967962739e9d7efe5af3564e5c6aa6
임한택_T7429 <143519383+LHANTAEK@users.noreply.github.com>
2024-12-23 23:19:28 +0900

    [Bronze V] Title: 새싹, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.py
@@ -0,0 +1,9 @@
+def print_sprout():
+    print("         ,r'\"7")
+    print("r`-_   ,'  ,/")
+    print(" \\. \". L_r'")
+    print("   `~\\/")
+    print("      |")
+    print("      |")
+    
+print_sprout()
