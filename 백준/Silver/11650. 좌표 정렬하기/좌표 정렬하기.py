2ef21de6ded0f75cbadaa78f544b23574a681bcd
임한택_T7429 <143519383+LHANTAEK@users.noreply.github.com>
2024-12-30 17:15:21 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 2896 ms, Memory: 56024 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -0,0 +1,11 @@
+N = int(input())
+answer_list = []
+
+for i in range(N):
+    X, Y = map(int, input().split())
+    answer_list.append([X,Y])
+
+answer = sorted(answer_list, key=lambda x: (x[0], x[1]))
+
+for l in answer:
+    print(f"{l[0]} {l[1]}")
