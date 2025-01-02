ac9ee27c99472a343b1f442e898a4fd374db229e
임한택_T7429 <143519383+LHANTAEK@users.noreply.github.com>
2025-01-02 16:25:15 +0900

    [Bronze II] Title: 음계, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,11 @@
+N = list(map(int, input().split()))
+
+ascending = [1,2,3,4,5,6,7,8]
+descending = [8,7,6,5,4,3,2,1]
+
+if N == ascending:
+    print("ascending")
+elif N == descending:
+    print("descending")
+else:
+    print("mixed")
