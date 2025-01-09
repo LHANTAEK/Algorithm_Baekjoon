975cdf611c08c9899cf7e24ce42f51f91471603d
임한택_T7429 <143519383+LHANTAEK@users.noreply.github.com>
2025-01-10 01:07:09 +0900

    [Bronze I] Title: FizzBuzz, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/28702. FizzBuzz/FizzBuzz.py
@@ -0,0 +1,15 @@
+ans = 0
+for i in [3, 2, 1]:
+    tmp = input().rstrip()
+
+    if tmp not in ['Fizz', 'Buzz', 'FizzBuzz']:
+        ans = int(tmp) + i
+        
+if ans % 3 == 0 and ans % 5 == 0:
+    print("FizzBuzz")
+elif ans % 3 == 0 and ans % 5 !=0:
+    print("Fizz")
+elif ans % 3 !=0 and ans % 5 ==0:
+    print("Buzz")
+else:
+    print(ans) 
