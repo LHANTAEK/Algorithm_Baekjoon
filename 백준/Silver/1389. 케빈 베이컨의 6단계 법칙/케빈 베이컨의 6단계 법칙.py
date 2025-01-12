ad63c9336ed4c6e4a4834fb2d50e09b4ec5df735
임한택_T7429 <143519383+LHANTAEK@users.noreply.github.com>
2025-01-12 21:44:18 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 168 ms, Memory: 34984 KB -BaekjoonHub

---
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
@@ -0,0 +1,46 @@
+from collections import deque
+
+N, M = map(int, input().split())
+adj = [[] for _ in range(N)]
+
+for _ in range(M):
+    a, b = map(lambda x:x-1, map(int, input().split())) # 제로베이스로 변환
+    adj[a].append(b)
+    adj[b].append(a)
+
+kevin = [] # 케빈 베이컨의 수를 넣을 리스트
+ans = (-1, float('inf'))
+
+def bfs(start, goal):
+    chk = [False] * N
+    chk[start] = True
+    dq = deque()
+    dq.append((start,0))
+    while dq:
+        now, d = dq.popleft()
+        if now == goal:
+            return d
+        
+        nd = d + 1
+        for nxt in adj[now]:
+            if not chk[nxt]:
+                chk[nxt] = True
+                dq.append((nxt, nd))
+
+def get_kevin(start):
+    tot = 0
+    
+    for i in range(N):
+        if i != start:
+            tot += bfs(start, i)
+    
+    return tot
+
+for i in range(N):
+    kevin.append(get_kevin(i))
+
+for i, v in enumerate(kevin):
+    if ans[1] > v:
+        ans = (i, v)
+        
+print(ans[0]+1)
