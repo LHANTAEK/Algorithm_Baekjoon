8fb69f4ba6dde679cf5669ff44a3a66ab55cfa47
임한택_T7429 <143519383+LHANTAEK@users.noreply.github.com>
2025-01-19 21:15:29 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 696 ms, Memory: 66160 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.py
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.py
@@ -3,17 +3,18 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 result = [[int(x) for x in input().rstrip()] for _ in range(n)]
 
+# dp 만들기
 dp = [[0] * m for _ in range(n)]
+
+# 첫행 첫열은 result와 똑같이
 dp[0] = result[0]
 
 for i in range(n):
     dp[i][0] = result[i][0]
     
-for i in range(1, n):
+for i in range(1,n):
     for j in range(1, m):
         if result[i][j] == 1:
             dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
 
-# 한 변의 길이를 구한 후 넓이를 계산
-side_length = max(map(max, dp))
-print(side_length * side_length)
+print(max(map(max, dp))**2)
